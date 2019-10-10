e317d48b44751e2ef549e6c52787d406edad1e4b
ArunKumarMavjay <arunkumar@vijayasekar.com>
2019-10-10 11:50:41 +0530

    Overcome the some Vulnerability issues in Gamble

--- contracts/BigGamble.sol
+++ contracts/BigGamble.sol
@@ -1,6 +1,3 @@
-
-
-
 pragma solidity >=0.4.22 <0.6.0;
 contract BigGamble {
 
@@ -35,7 +32,33 @@ contract BigGamble {
     constructor() public{
        
     }
-   
+
+function mul(uint256 a, uint256 b) internal pure returns (uint256) 
+{
+uint256 c = a * b;
+require(a == 0 || c / a == b);
+return c;
+}
+function div(uint256 a, uint256 b) internal pure returns (uint256) 
+{
+// assert(b > 0); // Solidity automatically throws when dividing by 0
+require(b>0);
+uint256 c = a / b;
+// assert(a == b * c + a % b); // There is no case in which this doesn't hold
+return c;
+}
+function sub(uint256 a, uint256 b) internal pure returns (uint256) 
+{
+assert(b <= a);
+return a - b;
+}
+function add(uint256 a, uint256 b) internal pure returns (uint256) 
+{
+uint256 c = a + b;
+require(c >= a);
+return c;
+}
+
     function checkPlayerExists(address playerAddress) public view returns(bool){
       for(uint256 i = 0; i < bettorInfo.length; i++){
          if(bettorInfo[i].player == playerAddress) return true;
@@ -74,7 +97,7 @@ contract BigGamble {
 
     }
     function calculateWizardRatio(uint wizardSOT,uint wizardTOB) public pure returns(uint){
-        uint twizardRatio = wizardTOB/wizardSOT;
+        uint twizardRatio = div(wizardTOB,wizardSOT);
         return twizardRatio;
     }
 
@@ -87,21 +110,27 @@ contract BigGamble {
     function percent(uint numerator, uint denominator, uint precision) public pure returns(uint quotient) {
 
          // caution, check safe-to-multiply here
-        uint _numerator  = numerator * 10 ** (precision+1);
+	uint256 pre = add(precision,1);
+	uint256 Exponentiation= 10**pre;
+	uint _numerator  = mul(numerator,Exponentiation);
         // with rounding of last digit
-        uint _quotient =  ((_numerator / denominator) + 5) / 10;
+	uint256 div_numerator = div(_numerator , denominator);
+	uint256 add_number = add(div_numerator,5);
+	uint _quotient =  div(add_number,10);
+       // uint _quotient =  ((_numerator / denominator) + 5) / 10;
         return ( _quotient);
     }
 
     function calculatePowerRatio(uint wizardPower,uint tPWizards) public pure returns(uint){
-         uint tpowerRatio = tPWizards/wizardPower;
+         uint tpowerRatio = div(tPWizards,wizardPower);
         return tpowerRatio;
     }
    
     function calculateStandardizedBet(uint betAmt,uint powerRatio,uint wizardRatio) public pure returns(uint){
        
      //  uint256 result = mod()
-        return betAmt*powerRatio*wizardRatio;
+         uint256 Ratio = mul(powerRatio,wizardRatio);
+	 return mul(betAmt,Ratio);
     }
    
    
@@ -134,11 +163,8 @@ contract BigGamble {
        
     }
    
-    // function getBalance() public view returns (uint256) {
-    //     return address(this).balance;
-    // }
-    function getAllInfoOfAUser(address playerAddress) public {
-       
+    function getAllInfoOfAUser(address playerAddress) public 
+	{   
        for (uint i = 0;i< bettorInfo.length;i++){
         if (bettorInfo[i].player == playerAddress) {
             emit throwWizardInfo(bettorInfo[i].selectedWizardId,calculateTotalBetAmoutnOnThisWizard(bettorInfo[i].selectedWizardId));
